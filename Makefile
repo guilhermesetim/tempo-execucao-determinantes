fad21b432c22850e70578219d7c1035a25c4e412
Guilherme Setim <64696390+guilhermesetim@users.noreply.github.com>
2023-01-14 21:01:42 -0300

    inserir matrizes por arquivo CSV

--- Makefile
+++ Makefile
@@ -3,11 +3,13 @@ all: compiliar linkar
 run:
 	./bin/main.out
 
+clean:
+	rm ./obj/*.o ./bin/*.out
+
 compiliar:
 	g++ -c ./src/main.cpp -o ./obj/main.o
 	g++ -c ./src/Sarrus.cpp -o ./obj/Sarrus.o
 	g++ -c ./src/Triangulo.cpp -o ./obj/Triangulo.o
-	g++ -c ./src/Jacob.cpp -o ./obj/Jacob.o
 	g++ -c ./src/Laplace.cpp -o ./obj/Laplace.o
 	g++ -c ./src/Chio.cpp -o ./obj/Chio.o
 	g++ -c ./src/Gauss.cpp -o ./obj/Gauss.o
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <chrono>
+#include <fstream>
 
 #include "../include/Sarrus.hpp"
 #include "../include/Triangulo.hpp"
-#include "../include/Jacob.hpp"
 #include "../include/Laplace.hpp"
 #include "../include/Chio.hpp"
 #include "../include/Gauss.hpp"
@@ -11,8 +11,10 @@
 using namespace std::chrono;
 using namespace std;
 
-int main() {
+void retornaConteudoArquivo(string _nomeArquivo, float m[4][4]);
 
+int main() {
+/*
     float matriz1[3][3] = { 
         {4,5,-3},
         {2,1,0},
@@ -55,54 +57,86 @@ int main() {
         {3,1,1,5},
         {2,2,1,1} 
     };
-
-    Laplace laplace1(matrizLaplace);
-    Laplace laplace2(matrizChio);
-    Laplace laplace3(matrizGauss);
-
-    Chio chio1(matrizLaplace);
-    Chio chio2(matrizChio);
-    Chio chio3(matrizGauss);
-
-    Gauss gauss1(matrizLaplace);
-    Gauss gauss2(matrizChio);
-    Gauss gauss3(matrizGauss);
+*/
     
-    
-    
-    
+    float m[4][4], mL[4][4], mC[4][4], mG[4][4];
+    retornaConteudoArquivo("./arquivosCSV/arquivo.csv", m);
 
-    
-
-   
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            cout << m[i][j] << " ";
+            mL[i][j] = m[i][j];
+            mC[i][j] = m[i][j];
+            mG[i][j] = m[i][j];
+        }
+        cout << endl;
+    }
 
 
     auto start = chrono::high_resolution_clock::now();
-
-    Sarrus sarrus1(matriz1);
-    //cout << "Determinante: " << sarrus1.getResultado() << endl;
-    Sarrus sarrus2(matriz2);
-    //cout << "Determinante: " << sarrus2.getResultado() << endl;
-
-
+    Laplace laplace1(mL);
     auto stop = high_resolution_clock::now();
-    std::chrono::nanoseconds duracao = duration_cast<nanoseconds>(stop - start);
-
-    //cout << "Duracao Sarrus: " << duracao.count() << endl;
+    std::chrono::nanoseconds duracaoLaplace = duration_cast<nanoseconds>(stop - start);
+    cout << "Duracao Laplace: " << duracaoLaplace.count() << endl;
+    
 
     start = chrono::high_resolution_clock::now();
-    Triangulo triangulo1(matriz1);
-    //cout << "Determinante: triangulo: " << triangulo1.getResultado() << endl;
-    Triangulo triangulo2(matriz2);
-    //cout << "Determinante: triangulo: " << triangulo2.getResultado() << endl;
+    Chio chio1(mC);
     stop = high_resolution_clock::now();
-    std::chrono::nanoseconds duracaoT = duration_cast<nanoseconds>(stop - start);
+    std::chrono::nanoseconds duracaoChio = duration_cast<nanoseconds>(stop - start);
+    cout << "Duracao Chio: " << duracaoChio.count() << endl;
 
-    //cout << "Duracao Triangulo: " << duracaoT.count() << endl;
 
-
+    start = chrono::high_resolution_clock::now();
+    Gauss gauss1(mG);
+    stop = high_resolution_clock::now();
+    std::chrono::nanoseconds duracaoGauss = duration_cast<nanoseconds>(stop - start);
+    cout << "Duracao Gauss: " << duracaoGauss.count() << endl;
     
+       
 
 
     return 0;
+}
+
+
+void retornaConteudoArquivo(string _nomeArquivo, float m[4][4])
+{
+    std::ifstream leitura(_nomeArquivo.c_str()); //abre o arquivo
+   
+
+    if(leitura)
+    {
+      
+        for(int i = 0; i < 4; i++) {
+
+            string linha, valorTabela = "";
+            int caracter = 0, colM = 0;
+            getline(leitura, linha);
+            
+            while(caracter < linha.size()) {
+                if(linha[caracter] != ',') {
+                    valorTabela+= linha[caracter];
+                }
+                else {
+                    
+                    m[i][colM] = stoi(valorTabela);
+                    ++colM;
+                    valorTabela = "";
+
+                }
+
+                ++caracter;
+            }
+
+            m[i][colM] = stoi(valorTabela);
+            colM = 0;
+        }
+    }
+    else
+        cout << "Arquivo não existe!!";
+
+
+    leitura.close(); //fecha o arquivo
+ 
 }
